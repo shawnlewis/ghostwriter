a80f6b3e8c56584caffe09aaffc3d7c526971df2
Shawn Lewis <shlewis@gmail.com>
2019-12-07 12:15:30 -0800

    Real editor with cursor

--- frontend/src/Editor.tsx
+++ frontend/src/Editor.tsx
@@ -3,6 +3,9 @@ import { useEffect, useRef, useState } from "react";
 
 import * as Api from "./api";
 
+const maxContext = 400;
+const maxGhostText = 400;
+
 const nextTabComplete = (text: string) => {
   const result = text.match(/([^\w]*[\w]+)/);
   if (result == null) {
@@ -12,8 +15,19 @@ const nextTabComplete = (text: string) => {
   return [tabComplete, text.slice(tabComplete.length)];
 };
 
-const maxContext = 400;
-const maxGhostText = 400;
+const renderNewLines = (text: string) => {
+  const lines = text.split("\n");
+  return (
+    <>
+      {lines.map((line, i) => (
+        <>
+          {line}
+          {i !== lines.length - 1 && <br />}
+        </>
+      ))}
+    </>
+  );
+};
 
 export const Editor: React.FC = () => {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
@@ -34,7 +48,7 @@ export const Editor: React.FC = () => {
     if (nextGhostText.length < maxGhostText) {
       setGhostText(nextGhostText.slice(0, maxGhostText));
     }
-  }, [response]);
+  }, [response, ghostText]);
 
   // Ghost text typing interval, active when we have some ghost
   // text
@@ -60,46 +74,48 @@ export const Editor: React.FC = () => {
     <div className="editor">
       <div ref={backdropRef} className="backdrop">
         <div className="highlights">
-          <span className="user-input">{input}</span>
-          <span className="suggestion">{tabComplete}</span>
-          <span className="ghost-input">{remainder}</span>
+          <span className="user-input">{renderNewLines(input)}</span>
+          <span className="suggestion">{renderNewLines(tabComplete)}</span>
+          <span className="ghost-input">{renderNewLines(remainder)}</span>
         </div>
       </div>
-      <textarea
-        ref={textAreaRef}
-        value={input}
-        onChange={e => {
-          const fullText = input + ghostText;
-          const newInput = e.target.value;
-          setInput(newInput);
-          if (
-            newInput.length < input.length ||
-            !fullText.startsWith(newInput)
-          ) {
-            // The user typed something new
-            setGhostIndex(0);
-            setGhostText("");
-          } else {
-            // The user typed exactly what's in the ghost text, so
-            // keep it.
-            setGhostText(ghostText.slice(newInput.length - input.length));
-          }
-        }}
-        onKeyDown={e => {
-          if (e.key === "Tab") {
-            // Tab complete
-            e.stopPropagation();
-            e.preventDefault();
-            setInput(input + tabComplete);
-            setGhostText(remainder);
-          }
-        }}
-        onScroll={() => {
-          if (textAreaRef.current != null && backdropRef.current != null) {
-            backdropRef.current.scrollTop = textAreaRef.current.scrollTop;
-          }
-        }}
-      ></textarea>
+      <div className="textarea">
+        <textarea
+          ref={textAreaRef}
+          value={input}
+          onChange={e => {
+            const fullText = input + ghostText;
+            const newInput = e.target.value;
+            setInput(newInput);
+            if (
+              newInput.length < input.length ||
+              !fullText.startsWith(newInput)
+            ) {
+              // The user typed something new
+              setGhostIndex(0);
+              setGhostText("");
+            } else {
+              // The user typed exactly what's in the ghost text, so
+              // keep it.
+              setGhostText(ghostText.slice(newInput.length - input.length));
+            }
+          }}
+          onKeyDown={e => {
+            if (e.key === "Tab") {
+              // Tab complete
+              e.stopPropagation();
+              e.preventDefault();
+              setInput(input + tabComplete);
+              setGhostText(remainder);
+            }
+          }}
+          onScroll={() => {
+            if (textAreaRef.current != null && backdropRef.current != null) {
+              backdropRef.current.scrollTop = textAreaRef.current.scrollTop;
+            }
+          }}
+        ></textarea>
+      </div>
     </div>
   );
 };
