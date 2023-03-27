06e8dbd72b4825a1fb68647c3baa6978dfd5943b
Shawn Lewis <shlewis@gmail.com>
2023-03-27 03:46:18 -0400

    Update run-prod.sh

--- server/run-prod.sh
+++ server/run-prod.sh
@@ -1,1 +1,1 @@
-gunicorn -w 4 -b 0.0.0.0:9911 'main:app'
+~/.pyenv/shims/gunicorn -w 1 -b 0.0.0.0:9911 'main:app' --error-logfile ~/error.log --access-logfile ~/access.log --capture-output --log-level debug
